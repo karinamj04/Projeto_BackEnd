ccab8df798e26e0d60dbe0d40e08de19b92ee532
Maria Julia <167549384+karinamj04@users.noreply.github.com>
2025-11-20 17:06:04 -0300

    Refactor sistema.php for improved structure and clarity

--- PROJETO.php/sistema.php
+++ PROJETO.php/sistema.php
@@ -366,10 +366,17 @@ $nomeUsuario = $_SESSION['nome'];
     <p>© 2025 Raízes da Saúde. Todos os direitos reservados.</p>
   </footer>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="js/agendar.js"></script>
+ 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script src="js/agendar.js"></script>
+
   
 
 </body>
 </html>
+ 
