0c34a01f7b597dfa184e48f8c6fc94418668880b
Arthur <133824941+chaesblue@users.noreply.github.com>
2025-11-26 19:45:29 -0300

    Update sobrenos.php
    corrigindo erro de compatibilidade com o css

--- PROJETO.php/sobrenos.php
+++ PROJETO.php/sobrenos.php
@@ -4,7 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/sobrenos.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script type="text/javascript" src="js/darkmode.js" defer></script>
+  <script type="text/javascript" src="js/controleFonte.js" defer></script>
   <title>Sobre Nós - Raízes da Saúde </title>
 <style>
   body {
@@ -25,19 +28,19 @@
 </head>
 <body>
 
-    <header>
-        <!------------------------------NOSSA LOGO------------------------->
-          <h2 class="logo">Raízes da Saúde</h2>
-  
-          <nav class="navigation">
-  
-            <ul class="nav nav-tabs">
-              <li class="nav-item">
-                <a class="nav-link" href="home.php">Home</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Especialidades</a>
-                <ul class="dropdown-menu">
+      <header>
+      <!------------------------------NOSSA LOGO------------------------->
+        <h2 class="logo">Raízes da Saúde</h2>
+
+        <nav class="navigation">
+
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <a class="nav-link" href="home.php">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Especialidades</a>
+              <ul class="dropdown-menu">
                 <li><a class="dropdown-item" href="quemsomos.php">Genicologista e Obstetrícia</a></li>
                 <li><a class="dropdown-item" href="quemsomos.php">Pediatria</a></li>
                 <li><a class="dropdown-item" href="quemsomos.php">Cardiologista</a></li>
@@ -45,7 +48,7 @@
                 <li><a class="dropdown-item" href="quemsomos.php">Dermatologista</a></li>
                 <li><a class="dropdown-item" href="quemsomos.php">Psiquiatria</a></li>
                  <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="login.php">Agende sua consulta</a></li>
+                <li><a class="dropdown-item" href="cadastro.php">Agende sua consulta</a></li>
              
               </ul>
             </li>
@@ -59,20 +62,27 @@
                 <li><a class="dropdown-item" href="quemsomos.php">Raio-x</a></li>
                 <li><a class="dropdown-item" href="quemsomos.php">Mapa</a></li>
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="login.php">Agende sua consulta</a></li>
-                </ul>
-              </li>
-              
-            
-              <li class="nav-item">
-                <a class="nav-link" href="quemsomos.php">Quem somos</a>
-              </li>
-  
-              </nav>
-  
-            <button id="cadastro">Login</button>
-            
-      </header>
+                <li><a class="dropdown-item" href="cadastro.php">Agende sua consulta</a></li>
+              </ul>
+            </li> 
+          
+            <li class="nav-item">
+              <a class="nav-link" href="sobrenos.php">Sobre Nós</a>
+            </li>
+            <button id="ModoEscuro"><i class="bi bi-moon-fill"></i><i class="bi bi-brightness-high-fill"></i></button>
+            </nav>
+
+          <a id="login" href="login.php">login</a>
+
+          
+    </header>
+
+   <!--Botôes de aumentar e diminuir fonte-->
+    <div class="font-buttons">
+        <button class="button-alterFont" onclick="adjustFontSize(1)">A+</button>
+        <button class="button-alterFont" onclick="adjustFontSize(-1)">A-</button>
+    </div>
+
   <!-- CONTEÚDO -->
   <div class="sobre-nos">
     <div class="texto">
@@ -114,18 +124,7 @@
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
 
-   <script>
-    // Quando a página carregar...
-    document.addEventListener("DOMContentLoaded", function () {
-      // Seleciona o botão pelo ID
-      const botao = document.getElementById("cadastro");
-
-      // Adiciona o evento de clique
-      botao.addEventListener("click", function () {
-        // Redireciona para a outra página
-        window.location.href = "login.php";
-      });
-    });
+  
   </script>
 </body>
 </html>
